e76ee906d06f080a52bd84d326b347e0eff32d84
Raghav Dhir <raghavdhir1510@gmail.com>
2024-11-14 00:47:52 +0530

    portfolio website

---
+++ script.js
@@ -0,0 +1,17 @@
+// Smooth Scrolling to Sections
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  // Placeholder functions for button actions
+  function seeResume() {
+    window.open("https://drive.google.com/file/d/1SWLNA_ShgS3ap13_azcfkEBiSM2pXsnW/view?usp=sharing", "_blank");
+  }
+  
+  function viewProject() {
+    alert("Project code viewing feature coming soon!");
+  }
+  
+  function liveDemo() {
+    alert("Live demo feature coming soon!");
+  }
